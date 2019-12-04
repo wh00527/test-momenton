e052d22bcdf1132d7edd9f6de1d80a7c296f7fbf
Kevin <wh00527@gmail.com>
2019-12-05 01:35:50 +1100

    2019-12-05 [Dev] adding accurate test cases

--- test/index.js
+++ test/index.js
@@ -1,17 +1,24 @@
-const employee = require("../lib/employee"),
+const checkRelation = require("../lib/employee"),
+      employees = require('../resource/source'),
       chai  = require("chai"),
       expect = chai.expect;
 
 
 describe('employee', () => {
     
-    describe('Constructor', () => {
+    describe('generation of relations ', () => {
 
         it('should be have correct result', () => {
-            const result = employee();
-            expect(result).to.have.property('items');
-            // expect(result).to.have.property('');
-            // expect(result).to.have.property('');
+            const result = checkRelation(employees);
+            expect(result[0]).to.have.property('manager_id');
+            expect(result[0].id).to.equal(150);
+            expect(result.length).to.equal(1);
+        });
+
+        it('should be have correct result with sub obj', () => {
+            const result = checkRelation(employees);
+            //should return 2 sub obj
+            expect(result[0].children.length).to.equal(2);
         });
     });    
 });
